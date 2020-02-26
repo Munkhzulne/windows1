40dc1208800f9dc20436353d8ec62949a6254415
Munkhzul <munkhzulnee@gmail.com>
2020-02-26 18:06:57 +0800

    done

---
+++ Windows1/Task1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Munkhzul
+{
+    namespace Lab1
+    {
+        class Class1
+        {
+            public Class1()
+            {
+            }
+            public void readFromNamespaceLab1()
+            {
+                Console.WriteLine("Ymar ng zuil bichne uu:");
+                String msg;
+                msg=Console.ReadLine();
+                Console.WriteLine("Ta uuniig bichlee: " + msg);
+            }
+        }
+    }
+}
